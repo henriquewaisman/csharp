9022151eb288fd88e438d7a6d2714e19a6644e5b
henriquewaisman <howaisman@gmail.com>
2025-04-11 14:09:24 -0300

    Modela classe Filme e FilmeController

--- csharp-alura/FilmesApi/obj/Debug/net8.0/FilmesApi.AssemblyInfo.cs
+++ csharp-alura/FilmesApi/obj/Debug/net8.0/FilmesApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9f2bab4e83fd5052af4785cc1ce2c93e841b053")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a77d52e0cdf91290db49760e50f8e18553c377")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
