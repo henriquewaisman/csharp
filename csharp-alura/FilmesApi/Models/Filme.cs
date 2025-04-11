f50f86721d76f372d07497a6b7f73edd495b4850
henriquewaisman <howaisman@gmail.com>
2025-04-11 15:58:45 -0300

    Adiciona o tratamento dos atributos de filme, adciona gitignore

--- csharp-alura/FilmesApi/Models/Filme.cs
+++ csharp-alura/FilmesApi/Models/Filme.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FilmesApi.Models;
 
 public class Filme
 {
+    [Required]
+    [MaxLength(255, ErrorMessage = "Tamanho de título inválido.")]
     public string Titulo { get; set; }
+    [Required]
+    [MaxLength(255, ErrorMessage = "Tamanho de título inválido.")]
     public string Genero { get; set; }
+    [Required]
+    [Range(70, 600, ErrorMessage = "Duração deve ter entre 60 e 700 minutos.")]
     public int Duracao { get; set; }
 }
--- csharp-alura/FilmesApi/obj/Debug/net8.0/FilmesApi.AssemblyInfo.cs
+++ csharp-alura/FilmesApi/obj/Debug/net8.0/FilmesApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a77d52e0cdf91290db49760e50f8e18553c377")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9022151eb288fd88e438d7a6d2714e19a6644e5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
